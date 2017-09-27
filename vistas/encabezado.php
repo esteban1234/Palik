a219a680e7678b19b632e6d110ed6d8bdb7874e4
esteban1234 <tva_libra@hotmail.com>
2017-09-27 16:27:07 -0500

    metas

--- vistas/encabezado.php
+++ vistas/encabezado.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta name="google-site-verification" content="ojnmvjKXupORzpfF4T_B4CMxiD8tTyTK2v6r417EXlQ" />
 	<link rel="icon" type="image/png" href="img/favicon.png" />
 	<title>ADMINISTRADORA PALIK</title>
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
